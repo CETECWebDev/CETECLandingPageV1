84c0bacc539c6b850f5a22701ded5ddec15045f0
winchester-cell <manili.mm91@gmail.com>
2025-05-14 12:27:29 +0330

    merged

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Services from './components/Services/Services';
-import Questions from './components/Questions/Questions';
+import Services from "./components/Services/Services";
+import Questions from "./components/Questions/Questions";
 
 export default function Home() {
   return (
     <>
       <Services />
-      <Questions />
+      <Questions /> 
     </>
-  );
+  )
 }
