f33d7156fc1dc760ef44fd3a4a66fe86d4539869
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-14 10:25:28 +0330

    padding header removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
 // Import the cart component
 import React from "react";
-import HeroSection from "@/components/HeroSection/HeroSection";
-import CardSection from "@/components/CardSection/CardSection";
 import Header from "@/components/Header/Header";
 import MainTemplate from "@/components/MainTemplate/MainTemplate";
 
 export default function Home() {
   return (
     <>
-      <div className="container px-5">
+      
         <Header />
         <MainTemplate />
        
 
         
-      </div>
+    
     </>
   );
 }
