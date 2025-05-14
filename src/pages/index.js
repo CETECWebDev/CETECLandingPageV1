513d5217183bf591df7095882d8791c772944675
winchester-cell <manili.mm91@gmail.com>
2025-05-14 09:59:18 +0330

    adjusment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import MainTemplate from '@/components/MainTemplate/MainTemplate';
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Header />
       <MainTemplate />
+      <Footer />
     </>
   )
 }
