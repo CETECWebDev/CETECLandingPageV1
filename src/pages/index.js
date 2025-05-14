da7d76fb0c8ea9e47bfd56c36a94c51062dd8fd6
winchester-cell <manili.mm91@gmail.com>
2025-05-14 10:36:49 +0330

    tailwindfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
-import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import MainTemplate from '@/components/MainTemplate/MainTemplate';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <MainTemplate />
-      <Footer />
     </>
   )
 }
