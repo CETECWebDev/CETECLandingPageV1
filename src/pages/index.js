d60f45c83b7ea9e03e1d5d4d00362b7fa0c010b1
winchester-cell <manili.mm91@gmail.com>
2025-05-14 13:25:29 +0330

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import React from "react";
-import Header from "@/components/Header/Header";
 import MainTemplate from "@/components/MainTemplate/MainTemplate";
 
 export default function Home() {
   return (
     <>
-    
+
       <Header />
       <MainTemplate />
       <Footer />
