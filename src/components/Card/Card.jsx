5be9a0c0cef0d57b7feab85972c4f07e3f8128f4
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-17 14:05:00 +0330

    ui bug fixed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Card( { image, title, description }) {
   return (
-    <div className="pb-4 overflow-hidden bg-white rounded-lg shadow-md text-center">
+    <div className=" overflow-hidden bg-white rounded-lg shadow-md text-center">
       <div className="">
         <img
           src={image} // Replace with the actual image path
@@ -11,9 +11,9 @@ export default function Card( { image, title, description }) {
         />
       </div>
       <h3 className="text-lg pt-6 font-bold text-[#3A3A3A] mb-2 font-[YekanBakhBold]">{title}</h3>
-      <p className="text-sm pt-6 pb-12  text-[#3A3A3A] font-[YekanBakh]">
+      <p className="text-sm pt-2 px-1 text-justify flex justify-center lg:px-2 lg:pt-6 pb-5  lg:pb-8 text-[#3A3A3A] font-[YekanBakh]">
       {description}
-      </p>
+      </p> 
     </div>
   );
 }
